a0640ac11e6ea2927290c5e711ce50aaddc752e4
George Fabish <georgefabish@gmail.com>
2022-10-22 09:26:43 -0400

    Added singleton script template

--- Editor/ScriptAssetGenerator.cs
+++ Editor/ScriptAssetGenerator.cs
@@ -7,29 +7,37 @@ namespace GeorgeFabish.Editor
 {
     public class ScriptAssetGenerator
     {
-        private static readonly string _templateFolder = Path.Combine("Packages", "com.george.fabish.tools", "Editor","ScriptTemplates");
+        private static readonly string _templateFolder = Path.Combine("Packages", "com.george.fabish.tools", "Editor", "ScriptTemplates");
         private static readonly string _emptyClassTemplatePath = Path.Combine(_templateFolder, "EmptyClass.txt");
-        private static readonly string _emptyInterfaceTemplatePath = Path.Combine(_templateFolder, "EmptyInterface.txt");
+        private static readonly string _emptyInterfaceTemplatePath = Path.Combine(_templateFolder, "Interface.txt");
+        private static readonly string _singletonTemplatePath = Path.Combine(_templateFolder, "Singleton.txt");
 #if UNITY_EDITOR
         [MenuItem("Assets/Script Templates/C# Empty Class")]
         public static void CreateEmptyClass()
         {
             string filePath = GetFilePathFromUser("Create Empty C# Class", "EmptyClass");
-            if (!string.IsNullOrEmpty(filePath))
-            {
-                string template = File.ReadAllText(_emptyClassTemplatePath);
-                template = template.Replace("$className", Path.GetFileNameWithoutExtension(filePath));
-                File.WriteAllText(filePath, template);
-                AssetDatabase.Refresh();
-            }
+            CreateFile(filePath, _emptyClassTemplatePath);
+
         }
-        [MenuItem("Assets/Script Templates/C# Empty Interface")]
+        [MenuItem("Assets/Script Templates/C# Interface")]
         public static void CreateEmptyInterface()
         {
             string filePath = GetFilePathFromUser("Create Empty C# Interface", "IEmpty");
+            CreateFile(filePath, _emptyInterfaceTemplatePath);
+
+        }
+        [MenuItem("Assets/Script Templates/Singleton")]
+        public static void CreateSingleton()
+        {
+            string filePath = GetFilePathFromUser("Create Singleton", "Manager");
+            CreateFile(filePath, _singletonTemplatePath);
+        }
+
+        private static void CreateFile(string filePath, string templateFilePath)
+        {
             if (!string.IsNullOrEmpty(filePath))
             {
-                string template = File.ReadAllText(_emptyInterfaceTemplatePath);
+                string template = File.ReadAllText(templateFilePath);
                 template = template.Replace("$className", Path.GetFileNameWithoutExtension(filePath));
                 File.WriteAllText(filePath, template);
                 AssetDatabase.Refresh();
